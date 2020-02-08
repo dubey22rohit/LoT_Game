66986f010187f6ef23ba904682f17004d933d97f
Rohit Dubey <dubey22rohit@gmail.com>
2020-02-08 20:43:53 +0530

    Reset Button Added

--- app/src/main/java/com/skorpion/lotgame/MainActivity.java
+++ app/src/main/java/com/skorpion/lotgame/MainActivity.java
@@ -1,9 +1,11 @@
 package com.skorpion.lotgame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,24 +17,33 @@ enum Player{
 Player currentPlayer = Player.ONE;
 Player[] playerChoice = new Player[9];
 int[][] winRC = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+boolean gameOver = false;
+    private Button btnReset;
+    private GridLayout gridLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playerChoice[0] = Player.NO;
-        playerChoice[1] = Player.NO;
-        playerChoice[2] = Player.NO;
-        playerChoice[3] = Player.NO;
-        playerChoice[4] = Player.NO;
-        playerChoice[5] = Player.NO;
-        playerChoice[6] = Player.NO;
-        playerChoice[7] = Player.NO;
-        playerChoice[8] = Player.NO;
+
+        for(int i = 0;i<playerChoice.length;i++){
+            playerChoice[i] = Player.NO;
+        }
+        btnReset = findViewById(R.id.btnReset);
+        gridLayout = findViewById(R.id.gridView);
+
     }
     public void imgTap(View imageView){
         View imgTapped = (ImageView) imageView;
         int tiTag = Integer.parseInt(imgTapped.getTag().toString());
-        if(playerChoice[tiTag] == Player.NO) {
+
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            resetGame();
+            }
+        });
+        if(playerChoice[tiTag] == Player.NO && gameOver == false) {
             imgTapped.setTranslationX(-2000);
 
             playerChoice[tiTag] = currentPlayer;
@@ -48,6 +59,8 @@ int[][] winRC = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}
 
             for (int[] winnerCols : winRC) {
                 if (playerChoice[winnerCols[0]] == playerChoice[winnerCols[1]] && playerChoice[winnerCols[1]] == playerChoice[winnerCols[2]] && playerChoice[winnerCols[0]] != Player.NO) {
+                    btnReset.setVisibility(View.VISIBLE);
+                    gameOver = true;
                     String winner = "";
                     if(currentPlayer == Player.ONE){
                         winner = "Player Two";
@@ -56,8 +69,23 @@ int[][] winRC = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}
                         winner = "Player One";
                     }
                     Toast.makeText(this,winner + " Winner Weiner Chimken Dinner!",Toast.LENGTH_LONG).show();
+
+
                 }
             }
         }
+    }
+    private void resetGame(){
+    for(int i = 0;i<gridLayout.getChildCount();i++){
+        ImageView imageView = (ImageView)gridLayout.getChildAt(i);
+        imageView.setImageDrawable(null);
+        imageView.setAlpha(0.2f);
+    }
+        currentPlayer = Player.ONE;
+            for(int i = 0;i<playerChoice.length;i++){
+            playerChoice[i] = Player.NO;
+        }
+        gameOver = false;
+        btnReset.setVisibility(View.INVISIBLE);
     }
 }
